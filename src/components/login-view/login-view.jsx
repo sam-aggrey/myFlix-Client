4aca075ba43ee7feda8c312cdb5d772699123650
sam-aggrey <78800769+sam-aggrey@users.noreply.github.com>
2021-07-16 18:41:54 +0000

    Added proptypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -29,3 +29,10 @@ export function LoginView(props) {
     </Form>
   );
 }
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired
+};
