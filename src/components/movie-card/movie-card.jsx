8bd96a2c39f35207875a756d5768facc0440468f
sam aggrey <samaggrey@hotmail.com>
2021-07-21 17:28:35 +0000

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class MovieCard extends React.Component {
+
+export class MovieView extends React.Component {
+    
   render() {
-    const { movie, onMovieClick } = this.props;
-     
+    const { movie, onBackClick } = this.props;
+      console.log(movie);
+
     return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+         
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+    
+      
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
+      </div>
     );
   }
 }
-
-MovieCard.propTypes = {
+MovieView.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired
     }),
     Director: PropTypes.shape({
-      Name: PropTypes.string,
+      Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired
+      Birth: PropTypes.string
     }),
-    ImagePath: PropTypes.string,
-    Featured: PropTypes.bool,
-    
-  }).isRequired
+  }),
+  onBackClick: PropTypes.func.isRequired
 };
 
