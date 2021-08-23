157dc397805834ebcee645975e6c52aefb21c52f
sam aggrey <samaggrey@hotmail.com>
2021-08-23 08:21:37 +0000

    Update login-view.jsx
    Added a server authentication request code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    // Send a request to the server for authentication, then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
-  };
+  e.preventDefault();
+  /* Send a request to the server for authentication */
+  axios.post('https://sammy-flix.herokuapp.com/users', {
+    Username: username,
+    Password: password
+  })
+  .then(response => {
+    const data = response.data;
+    props.onLoggedIn(data);
+  })
+  .catch(e => {
+    console.log('no such user')
+  });
+};
 
     return (
      <Form>
@@ -29,3 +39,21 @@ export function LoginView(props) {
     </Form>
   );
 }
+
+//import axios from 'axios';
+//
+//const handleSubmit = (e) => {
+//  e.preventDefault();
+//  /* Send a request to the server for authentication */
+//  axios.post('https://sammy-flix.herokuapp.com/login', {
+//    Username: username,
+//    Password: password
+//  })
+//  .then(response => {
+//    const data = response.data;
+//    props.onLoggedIn(data);
+//  })
+//  .catch(e => {
+//    console.log('no such user')
+//  });
+//};
