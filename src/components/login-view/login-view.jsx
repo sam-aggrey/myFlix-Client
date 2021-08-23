c589c68304110a2b84539ce7a27e55f171d46815
sam aggrey <samaggrey@hotmail.com>
2021-08-23 18:08:28 +0000

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,59 +1,63 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import './login-view.scss';
 
 export function LoginView(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const handleSubmit = (e) => {
-  e.preventDefault();
-  /* Send a request to the server for authentication */
-  axios.post('https://sammy-flix.herokuapp.com/users', {
-    Username: username,
-    Password: password
-  })
-  .then(response => {
-    const data = response.data;
-    props.onLoggedIn(data);
-  })
-  .catch(e => {
-    console.log('no such user')
-  });
-};
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-     <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Send a request to the server for authentication */
+    axios.post('https://sammy-flix.herokuapp.com/users', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+  };
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
+  return (
+    <div className="login">
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Please provide your password</Form.Control.Feedback>
+        </Form.Group>
+        <span>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Log in</Button>
+          {' '}
+          <Link to={`/register`}>
+            <Button variant="success link">Register</Button>
+          </Link>
+        </span>
+      </Form>
+    </div>
   );
 }
 
-//import axios from 'axios';
-//
-//const handleSubmit = (e) => {
-//  e.preventDefault();
-//  /* Send a request to the server for authentication */
-//  axios.post('https://sammy-flix.herokuapp.com/login', {
-//    Username: username,
-//    Password: password
-//  })
-//  .then(response => {
-//    const data = response.data;
-//    props.onLoggedIn(data);
-//  })
-//  .catch(e => {
-//    console.log('no such user')
-//  });
-//};
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired
+};
