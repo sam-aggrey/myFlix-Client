ae8344667365d4b06cfe8e5ef3ac5d932628d3bb
sam aggrey <samaggrey@hotmail.com>
2021-08-30 13:46:54 +0000

    Update login-view.jsx
    Changed the endpoint

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,7 +14,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post('https://sammy-flix.herokuapp.com/users', {
+    axios.post('https://sammy-flix.herokuapp.com/login', {
       Username: username,
       Password: password
     })
