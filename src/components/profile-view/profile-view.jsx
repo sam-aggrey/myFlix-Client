0955dd6ee5993e326fc5ef1ae1677ab8a3f0070f
sam aggrey <samaggrey@hotmail.com>
2021-09-02 23:49:04 +0000

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -175,7 +175,7 @@ export class ProfileView extends React.Component {
                           <Card.Img style={{ width: '18rem' }} className="movieCard" variant="top" src={movie.ImageURL} />
                           <Card.Body>
                             <Card.Title className="movie-card-title">{movie.Title}</Card.Title>
-                            <Button size='sm' className='profile-button remove-favorite' variant='danger' value={movie._id} onClick={() => this.removeFavouriteMovie(movie)}>
+                            <Button size='sm' className='profile-button remove-favorite' variant='danger' value={movie._id} onClick={(e) => this.removeFavouriteMovie(e, movie)}>
                               Remove
                             </Button>
                           </Card.Body>
