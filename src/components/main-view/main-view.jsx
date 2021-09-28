b1beaa7e1e2d8b3191afba9323ab5731d8e5742c
sam aggrey <samaggrey@hotmail.com>
2021-09-28 10:55:19 +0000

    Update main-view.jsx
    Commented out a duplicated tag

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -129,11 +129,11 @@ export class MainView extends React.Component {
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
             </Col>
             if (movies.length === 0) return <div className="main-view" />;
-            return movies.map(m => (
+          /*  return movies.map(m => (
               <Col md={3} key={m._id}>
                 <MovieCard movie={m} />
               </Col>
-            ))
+            ))*/
             return <MoviesList movies={movies} />;
           }} />
 
